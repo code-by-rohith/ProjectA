67039fe0f2a2454f68b421ca4ed1d96b056d6ffb
code-by-rohith <rdrohith02@gmail.com>
2025-02-28 13:46:06 +0530

    eventlet3

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def handle_message(data):
     print(f"Received: {data}")
     socketio.send(f"Echo: {data}")
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8000, debug=True)
+# if __name__ == "__main__":
+#     socketio.run(app, host="0.0.0.0", port=8000, debug=True)
